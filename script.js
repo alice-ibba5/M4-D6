9ae3adc1bcaa3e1d830ee0f3ea72141915f8c961
alice-ibba5 <135819129+alice-ibba5@users.noreply.github.com>
2023-10-02 22:52:14 +0200

    finito

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-let research = document.querySelector("#input-query")
-let filter = document.querySelector("#select-one")
+let research = document.getElementById("input-query")
+let filter = document.getElementById("select-one")
 
 async function loadUsersData() {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/users`)
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users")
     const data = await response.json()
 
     return data
+  } catch (error) {
+    console.log(error)
+}
 }
 
 window.onload = async function () {
     const data = await loadUsersData()
-    const tbody = document.querySelector('tbody');
+    const tbody = document.querySelector("tbody");
     
     tbody.innerHTML = data
         .map((data) => {
           return  /*html*/`
             <tr class="contenitore">
-              <th>${data.id}</th>
+              <td>${data.id}</td>
               <td class="userName">${data.name}</td>
               <td class="userUsername">${data.username}</td>
               <td class="userEmail">${data.email}</td>
@@ -27,8 +31,8 @@ window.onload = async function () {
 }
 
 
-function searchData() {
-    let query = research.value.toLowerCase();
+/*const searchData = () => {
+    const query = research.value.toLowerCase();
     let filtro = filter.value;
     let allData = document.querySelectorAll(`.${filtro}`);
    
@@ -37,14 +41,48 @@ function searchData() {
       if (!(data.textContent.toLowerCase()).includes(query)) {
         currUser.style.display = "none";
       } else {
-        currUser.style.display = "";
+        currUser.style.display = "block";
       }
     })
   };
 
-  research.addEventListener('input', searchData);
-
-  filter.addEventListener('change', searchData);
-  
-  searchData()
  
+  research.addEventListener('input', searchData)
+
+  filter.addEventListener('change', searchData)*/
+  
+  
+  function filterResults() {
+    const input = research.value;
+    const user = document.getElementsByClassName('contenitore');
+    if (filter.value === 'name') {
+    let items = document.getElementsByClassName('userName');
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].innerHTML.toLowerCase().indexOf(input.toLowerCase()) > -1) {
+        user[i].style.display = '';
+      } else {
+        user[i].style.display = 'none';
+      }
+    }
+  } else if (filter.value === 'username') {
+    let items = document.getElementsByClassName('userUsername');    
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].innerHTML.toLowerCase().indexOf(input.toLowerCase()) > -1) {
+        user[i].style.display = '';
+      } else {
+        user[i].style.display = 'none';
+      }
+    }
+  } else if (filter.value === 'email') {
+    let items = document.getElementsByClassName('userEmail');    
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].innerHTML.toLowerCase().indexOf(input.toLowerCase()) > -1) {
+        user[i].style.display = '';
+      } else {
+        user[i].style.display = 'none';
+      }
+    }
+  }
+}
+  research.addEventListener('input', filterResults)
+
